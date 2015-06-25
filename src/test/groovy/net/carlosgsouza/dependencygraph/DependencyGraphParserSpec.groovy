d41d67f7e04833d7e012609ea9e4a2bb94f7cea0
carlosgsouza <carlosgsouza@gmail.com>
2015-06-24 21:04:46 -0700

    Design/Specs for Parser

--- src/test/groovy/net/carlosgsouza/dependencygraph/DependencyGraphParserSpec.groovy
+++ src/test/groovy/net/carlosgsouza/dependencygraph/DependencyGraphParserSpec.groovy
@@ -1,8 +1,99 @@
 package net.carlosgsouza.dependencygraph
 
+import java.io.File.TempDirectory;
+
 import spock.lang.Specification;
 
 class DependencyGraphParserSpec extends Specification {
 
+	DependencyGraphParser parser
 	
+	File tempFolder
+	
+	def setup() {
+		parser = new DependencyGraphParser()
+		createTempFolder()
+	}
+	
+	def cleanup() {
+		cleanTempFolder()
+	}
+	
+	def "should parse an input file and create a dependency graph"() {
+		given:
+		File inputFile = new File(tempFolder, "input.txt")
+		inputFile << """A->B
+A->C
+B->C
+B->D
+"""
+		when:
+		DependencyGraph dependencyGraph = parser.parse(inputFile.absolutePath)
+		
+		then:
+		dependencyGraph.root == "A"
+		
+		and:
+		dependencyGraph.dependencies["A"] == ["B", "C"]
+		dependencyGraph.dependencies["B"] == ["C", "D"]
+	}
+	
+	def "should ignore white spaces"() {
+		given:
+		File inputFile = new File(tempFolder, "input.txt")
+		inputFile << """
+			
+		A->B
+A	->	C
+
+
+"""
+		when:
+		DependencyGraph dependencyGraph = parser.parse(inputFile.absolutePath)
+		
+		then:
+		dependencyGraph.root == "A"
+		
+		and:
+		dependencyGraph.dependencies["A"] == ["B", "C"]
+	}
+	
+	def "should support special characters"() {
+		given:
+		File inputFile = new File(tempFolder, "input.txt")
+		inputFile << """
+			
+		#->á
+@	->	ç
+
+
+"""
+		when:
+		DependencyGraph dependencyGraph = parser.parse(inputFile.absolutePath)
+		
+		then:
+		dependencyGraph.root == "#"
+		
+		and:
+		dependencyGraph.dependencies["A"] == ["á"]
+		dependencyGraph.dependencies["@"] == ["ç"]
+	}
+	
+	
+	private void createTempFolder() {
+		tempFolder = new File("src/test/resources/temp/")
+		cleanTempFolder()
+		tempFolder.mkdirs()
+	}
+	
+	private void cleanTempFolder() {
+		if(tempFolder?.exists()) {
+			if(tempFolder.isDirectory()) {
+				tempFolder.deleteDir()
+			} else {
+				tempFolder.delete()
+			}
+		}
+	}
 }
+
--- src/test/groovy/net/carlosgsouza/dependencygraph/DependencyGraphSpec.groovy
+++ src/test/groovy/net/carlosgsouza/dependencygraph/DependencyGraphSpec.groovy
@@ -4,14 +4,20 @@ import spock.lang.Specification;
 
 class DependencyGraphSpec extends Specification {
 
-	def "should build a dependency graph given the dependencies between components"() {
-		when: "a new dependency graph is created"
-		DependencyGraph dependencyGraph = new DependencyGraph()
-		
-		then: "it contains no nodes"
+	DependencyGraph dependencyGraph
+	
+	def setup() {
+		dependencyGraph = new DependencyGraph()
+	}
+	
+	def "should build an empty dependency graph"() {
+		expect: 
 		dependencyGraph.root == null
-		
-		when: "dependencies are added"
+		dependencyGraph.dependencies == [:]
+	}
+	
+	def "should add dependencies to a dependency graph"() {
+		when: 
 		dependencyGraph.addDependency('A', 'B')
 		dependencyGraph.addDependency('B', 'C')
 		
@@ -21,30 +27,76 @@ class DependencyGraphSpec extends Specification {
 		and: "dependencies can be retrieved for a given node"
 		dependencyGraph.dependencies["A"] == ["B"]
 		dependencyGraph.dependencies["B"] == ["C"]
+	}
+	
+	def "should ignore duplicate dependencies"() {
+		given: 
+		dependencyGraph.addDependency('A', 'B')
 		
-		when: "repeated dependencies are added again"
-		dependencyGraph.addDependency('A', 'B')
-		dependencyGraph.addDependency('A', 'B')
-				
-		then: "repeated dependencies are not kept"
+		expect:
 		dependencyGraph.dependencies["A"] == ["B"]
 		
-		when: "circular dependencies are added, such as A->B->C->A"
+		when:
+		dependencyGraph.addDependency('A', 'B')
+		dependencyGraph.addDependency('A', 'B')
+		dependencyGraph.addDependency('A', 'B')
+		
+		then: 
+		dependencyGraph.dependencies["A"] == ["B"]
+	}
+	
+	def "should add circular dependencies without complaining"() {
+		given:
+		dependencyGraph.addDependency("A", "B")
+		dependencyGraph.addDependency("B", "C")
+		
+		when:
 		dependencyGraph.addDependency("C", "A")
 		
-		then: "the dependencyGraph does not complain"
+		then: 
 		dependencyGraph.dependencies["A"] == ["B"]
 		dependencyGraph.dependencies["B"] == ["C"]
 		dependencyGraph.dependencies["C"] == ["A"]
+	}
+	
+	def "should add unreachable nodes without complaining"() {
+		given:
+		dependencyGraph.addDependency("A", "B")
+		dependencyGraph.addDependency("B", "C")
 		
-		when: "unreachable depencies are added"
+		when: 
 		dependencyGraph.addDependency("X", "Y")
 		
-		then: "the dependencyGraph does not complain"
+		then: 
 		dependencyGraph.dependencies["A"] == ["B"]
 		dependencyGraph.dependencies["B"] == ["C"]
-		dependencyGraph.dependencies["C"] == ["A"]
 		dependencyGraph.dependencies["X"] == ["Y"]
+	}
+	
+	def "should consider nodes whose names have different cases different nodes"() {
+		given:
+		dependencyGraph.addDependency("A", "B")
+		
+		when:
+		dependencyGraph.addDependency("A", "a")
+		dependencyGraph.addDependency("a", "b")
+		dependencyGraph.addDependency("b", "B")
+		
+		then:
+		dependencyGraph.dependencies["A"] == ["B", "a"]
+		dependencyGraph.dependencies["a"] == ["b"]
+		dependencyGraph.dependencies["b"] == ["B"]
+	}
+	
+	def "should support special characters and words"() {
+		when:
+		dependencyGraph.addDependency("a.b.c", "#")
+		dependencyGraph.addDependency("#", "éã")
+		dependencyGraph.addDependency("junit:junit:4.12", "org.hamcrest:hamcrest-core:1.3")
+		
+		then:
+		dependencyGraph.dependencies["a.b.c"] == ["#"]
+		dependencyGraph.dependencies["#"] == ["éã"]
 	}
 	
 }
