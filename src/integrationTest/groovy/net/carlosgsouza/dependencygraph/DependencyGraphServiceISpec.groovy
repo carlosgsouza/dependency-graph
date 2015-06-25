ee18cc35c72c9d7419edddef8189eb744033d4cf
carlosgsouza <carlosgsouza@gmail.com>
2015-06-25 12:57:08 -0700

    Adding application code, integration tests and coverage

---
+++ src/integrationTest/groovy/net/carlosgsouza/dependencygraph/DependencyGraphServiceISpec.groovy
@@ -0,0 +1,82 @@
+package net.carlosgsouza.dependencygraph
+
+import spock.lang.Specification
+
+class DependencyGraphServiceISpec extends Specification {
+
+	def "should run the command line application"() {
+		when:
+		Process process = " ./build/install/dependency-graph/bin/dependency-graph.bat src/test/resources/input2.txt".execute()
+		
+		then:
+		process.text == """"A
+|_ B
+|  |_ C
+|  |  |_ E
+|  |     |_ H
+|  |     |  |_ L
+|  |     |     |_ I
+|  |     |        |_ O
+|  |     |        |  |_ P
+|  |     |        |     |_ Q
+|  |     |        |_ P
+|  |     |        |  |_ Q
+|  |     |        |_ K
+|  |     |           |_ N
+|  |     |           |_ L (!)
+|  |     |_ M
+|  |        |_ N
+|  |        |_ H
+|  |           |_ L
+|  |              |_ I
+|  |                 |_ O
+|  |                 |  |_ P
+|  |                 |     |_ Q
+|  |                 |_ P
+|  |                 |  |_ Q
+|  |                 |_ K
+|  |                    |_ N
+|  |                    |_ L (!)
+|  |_ D
+|     |_ F
+|     |  |_ H
+|     |     |_ L
+|     |        |_ I
+|     |           |_ O
+|     |           |  |_ P
+|     |           |     |_ Q
+|     |           |_ P
+|     |           |  |_ Q
+|     |           |_ K
+|     |              |_ N
+|     |              |_ L (!)
+|     |_ G
+|     |_ J
+|        |_ I
+|        |  |_ O
+|        |  |  |_ P
+|        |  |     |_ Q
+|        |  |_ P
+|        |  |  |_ Q
+|        |  |_ K
+|        |     |_ N
+|        |     |_ L
+|        |        |_ I (!)
+|        |_ Q
+|_ J
+   |_ I
+   |  |_ O
+   |  |  |_ P
+   |  |     |_ Q
+   |  |_ P
+   |  |  |_ Q
+   |  |_ K
+   |     |_ N
+   |     |_ L
+   |        |_ I (!)
+   |_ Q
+
+(!) Circular Dependency
+"""
+	}
+}
--- src/main/groovy/net/carlosgsouza/dependencygraph/DependencyGraphService.groovy
+++ src/main/groovy/net/carlosgsouza/dependencygraph/DependencyGraphService.groovy
@@ -15,4 +15,17 @@ class DependencyGraphService {
 		DependencyGraph dependencyGraph = parser.parse(dependencyGraphPath)
 		printer.print(dependencyGraph)
 	}
+	
+	public void run(String[] args) {
+		if(args == null || args.size() != 1) {
+			println "Exactly one parameter is required, the path of the input file"
+			return
+		}
+		
+		process(args[0])
+	}
+	
+	public static void main(String[] args) {
+		new DependencyGraphService().run(args)
+	}
 }
